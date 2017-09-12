938b5f5fa4608fd8893f14cb0417b5005c9992fd
SadEvilPenguin <mears.chris@gmail.com>
2017-09-12 12:25:15 -0400

    game functionality working. added reset functionality

--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,32 @@
 $(() => {
     //Game Logic Section
     const gameSequence = {
+        //keeps track of sequence
         sequence: [],
+        //function to create a number between 1 and 4 for the sequence
         rundomNumber: function() {
             return Math.ceil((Math.random() * 4));
         },
+        //keeps track of number of clicks
         clicked: 0,
+        //used to help with the lightUp Sequence
         lighted: 0,
     }
 
     //Game Content Update Section
+    //Reset Message
+    const resetGame = {
+        title: "Wrong one!",
+        description: "Looks you made it to Sqeuence: #. If you would like to see if you can beat your score please click Try Again.",
+        button: "Try Again"
+    };
+
+    //function to reload game
+    function loadText(text) {
+        $('#title').text(text.title);
+        $('#description').text(text.description);
+        $('#start').text(text.button);
+    };
 
     //array of lights classes for css
     const Lights = ["greenLight button", "redLight button", "yellowLight button", "blueLight button"];
@@ -20,47 +37,84 @@ $(() => {
     //controls the color change for the light
     function lightUp(i) {
         $('#' + i).attr('class', Lights[i - 1]);
+        //used timeout here to allow the light to stay on for a little while
         setTimeout(function() {
             $('#' + i).attr('class', nonLights[i - 1]);
         }, 200);
     }
 
-    //adds one random number to sequence then lights up areas of game by changing class
+    //runs the sequence for light up
     function runSequence() {
+        //timeout set up so that each ligth will go in sequence rather than lighting up together
         setTimeout(function() {
             lightUp(gameSequence.sequence[gameSequence.lighted]);
             gameSequence.lighted++;
+            //keep track of how many have been lighted in sequence if less than total it runs function again
             if (gameSequence.lighted < gameSequence.sequence.length) {
                 runSequence();
+                //if the sequence has finished this starts the listening function
             } else {
                 gameSequence.lighted = 0;
+                clickListener();
             }
         }, 1000);
     };
 
-    //function for listening for correct sequence of clicks
-    function clickListener() {
-
+    //starts next round by adding to sequence and starting runSequence function
+    function nextRound() {
+        gameSequence.sequence.push(gameSequence.rundomNumber());
+        runSequence();
     };
 
-    //function to starts game hide manual section change game to visible and shows it.
+    //function for listening for correct sequence of clicks
+    function clickListener() {
+        $('#1, #2, #3, #4').on('click', function() {
+            //animates light up on click
+            lightUp(this.id);
+            //if correct click
+            if (parseInt(this.id) === gameSequence.sequence[gameSequence.clicked]) {
+                //if this click is the last click
+                if (gameSequence.clicked === gameSequence.sequence.length - 1) {
+                    console.log('right');
+                    gameSequence.clicked = 0;
+                    //turns event listener off after click to prevent the click looping through the function
+                    $('#1, #2, #3, #4').off('click');
+                    nextRound();
+                    //if this click is not the last
+                } else {
+                    console.log('right');
+                    gameSequence.clicked++;
+                    //turns event listener off after click to prevent the click looping through the function
+                    $('#1, #2, #3, #4').off('click');
+                    clickListener();
+                }
+                //if this is the wrong click for the sequence
+            } else {
+                console.log('Wrong');
+                //turns event listener off after click to prevent the click looping through the function
+                $('#1, #2, #3, #4').off('click');
+                loadText(resetGame);
+                $('#game').hide();
+                $('#gameManual').show();
+                gameSequence.sequence = [];
+                gameSequence.clicked = 0;
+            }
+        })
+    };
+
+    //App Operation Section
+    //function to start game hide manual section change game to visible and shows it.
     function start() {
         $('#gameManual').hide();
         $('#game').attr('class', 'visible');
         $('#game').show();
         gameSequence.sequence.push(gameSequence.rundomNumber());
     };
-    //App Operation Section
+    //hides game at start
     $('#game').hide();
+    //waits for user to click start to iniate game
     $('#start').on('click', function() {
         start();
         runSequence();
-        $('#test').on('click', function() {
-            gameSequence.sequence.push(gameSequence.rundomNumber());
-            runSequence();
-        })
     });
-
-
-
 });
