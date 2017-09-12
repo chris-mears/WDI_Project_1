981495749c966118ad76541a7c1792d6ed06e502
SadEvilPenguin <mears.chris@gmail.com>
2017-09-12 09:15:52 -0400

    Got light up sequence working properly

--- JS/main.js
+++ JS/main.js
@@ -4,7 +4,9 @@ $(() => {
         sequence: [],
         rundomNumber: function() {
             return Math.ceil((Math.random() * 4));
-        }
+        },
+        clicked: 0,
+        lighted: 0,
     }
 
     //Game Content Update Section
@@ -15,36 +17,46 @@ $(() => {
     //array of nonlight classes for css
     const nonLights = ["green button", "red button", "yellow button", "blue button"];
 
+    //controls the color change for the light
     function lightUp(i) {
+        $('#' + i).attr('class', Lights[i - 1]);
         setTimeout(function() {
-            let index = gameSequence.sequence[i];
-            $('#' + index).attr('class', Lights[index - 1]);
-            setTimeout(function() {
-                $('#' + index).attr('class', nonLights[index - 1]);
-            }, 1000 * i);
-        }, 1000 * i);
+            $('#' + i).attr('class', nonLights[i - 1]);
+        }, 200);
     }
+
+    //adds one random number to sequence then lights up areas of game by changing class
+    function runSequence() {
+        setTimeout(function() {
+            lightUp(gameSequence.sequence[gameSequence.lighted]);
+            gameSequence.lighted++;
+            if (gameSequence.lighted < gameSequence.sequence.length) {
+                runSequence();
+            } else {
+                gameSequence.lighted = 0;
+            }
+        }, 1000);
+    };
+
+    //function for listening for correct sequence of clicks
+    function clickListener() {
+
+    };
 
     //function to starts game hide manual section change game to visible and shows it.
     function start() {
         $('#gameManual').hide();
         $('#game').attr('class', 'visible');
         $('#game').show();
-        runSequence();
-    }
-    //adds one random number to sequence then lights up areas of game by changing class
-    function runSequence() {
         gameSequence.sequence.push(gameSequence.rundomNumber());
-        for (let i = 0; i < gameSequence.sequence.length; i++) {
-            lightUp(i);
-        };
-        console.log(gameSequence.sequence);
-    }
+    };
     //App Operation Section
     $('#game').hide();
     $('#start').on('click', function() {
         start();
+        runSequence();
         $('#test').on('click', function() {
+            gameSequence.sequence.push(gameSequence.rundomNumber());
             runSequence();
         })
     });
